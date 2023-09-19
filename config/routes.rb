e55c943c42fd72ec29f48533472eacbbc56bc533
forLoop94 <charlesuwem94@gmail.com>
2023-09-19 09:40:33 +0100

    Fix delete category button in category show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :categories, only: [:index, :show] do
+  resources :categories, only: [:index, :show, :destroy] do
     resources :activities, only: [:index, :show]
   end
 
