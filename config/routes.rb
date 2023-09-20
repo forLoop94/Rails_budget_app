fcd0d58882fd4a82ab5d3005b198547cce3de185
forLoop94 <charlesuwem94@gmail.com>
2023-09-20 09:51:42 +0100

    Modify add new category page and new action to cat resources in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :categories, only: [:index, :show, :destroy] do
+  resources :categories, only: [:index, :show, :new, :create, :destroy] do
     resources :activities, only: [:index, :show]
   end
   root "categories#index"
