a45a971d4cec2cdfc67f955bf01c10094e71aec8
forLoop94 <charlesuwem94@gmail.com>
2023-09-20 09:56:14 +0100

    Add new and create functionalities to categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -22,11 +22,12 @@ class CategoriesController < ApplicationController
 
   # POST /categories or /categories.json
   def create
-    @category = Category.new(category_params)
+    @user = current_user
+    @category = @user.categories.new(category_params)
 
     respond_to do |format|
       if @category.save
-        format.html { redirect_to category_url(@category), notice: 'Category was successfully created.' }
+        format.html { redirect_to categories_path, notice: 'Category was successfully created.' }
         format.json { render :show, status: :created, location: @category }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -67,6 +68,6 @@ class CategoriesController < ApplicationController
 
   # Only allow a list of trusted parameters through.
   def category_params
-    params.fetch(:category, {})
+    params.require(:category).permit(:name, :icon)
   end
 end
